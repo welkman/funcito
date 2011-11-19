4fbcb95d34d2d93ff51f9b61500cc8c6aa4b1324
Kevin Welker <kbwelker@yahoo.com>
2011-11-18 21:49:25 -0600

    Removing old irrelevant experiments

--- oldExperimentSrc/factory2/FunctionWrapped.java
+++
@@ -1,9 +0,0 @@
-package factory2;
-
-import java.lang.annotation.*;
-
-@Target(value = {ElementType.METHOD})
-@Retention(value = RetentionPolicy.RUNTIME)
-public @interface FunctionWrapped {
-    public String value() default "";
-}
