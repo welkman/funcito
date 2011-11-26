8830617526a35048f5a4d4acb10c905f41ae8973
Michael Easter <codetojoy@gmail.com>
2011-11-26 16:35:16 -0400

    uses Google Guava Supplier support for 1 instance

---
+++ test/org/funcito/stub/StubFactory_UT.java
@@ -0,0 +1,18 @@
+package org.funcito.stub;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StubFactory_UT {
+	@Test
+	public void testInstance_CreatesOnlyOne() {
+		// test
+		StubFactory sf1 = StubFactory.instance();
+		StubFactory sf2 = StubFactory.instance();
+		StubFactory sf3 = StubFactory.instance();
+
+		assertEquals(sf1, sf2);
+		assertEquals(sf2, sf3);
+	}
+}
