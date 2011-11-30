1bfc5654639f419952fab9e8e1a29a3e448ca712
Kevin Welker <kandpwelker@gmail.com>
2011-11-29 23:14:47 -0600

    Repackaging example code to match the moved directory, and adding directory to IDEA .iml project file as a source dir

--- examples/org/funcito/example/MySubClass.java
+++ examples/org/funcito/example/MySubClass.java
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.funcito.exampleUsage;
+package org.funcito.example;
 
 public class MySubClass extends MyClass {
 
