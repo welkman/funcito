897f8450b0c2b2ffab57ad26a2fe642381dbc838
Kevin Welker <kandpwelker@gmail.com>
2011-12-17 21:06:41 -0600

    Oops. last checking accidentally had generic param.

--- oldExperimentSrc/mockStubs/MethodQueingReturnValues.java
+++ oldExperimentSrc/mockStubs/MethodQueingReturnValues.java
@@ -3,7 +3,7 @@ package mockStubs;
 import org.mockito.internal.stubbing.defaultanswers.ReturnsEmptyValues;
 import org.mockito.invocation.InvocationOnMock;
 
-public class MethodQueingReturnValues<T> extends ReturnsEmptyValues {
+public class MethodQueingReturnValues extends ReturnsEmptyValues {
 
     public Object answer(InvocationOnMock invocationOnMock) {
         MethodWrapper.pushMethod(invocationOnMock.getMethod());
