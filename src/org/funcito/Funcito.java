969e3c9601a6f09046954bb59de0e79caf2d97f3
Kevin Welker <kandpwelker@gmail.com>
2011-12-31 16:38:59 -0600

    added Funcito.callsTo(), for cleaner (more terse) use of the Funcito class, and added needed method Javadoc

--- src/org/funcito/Funcito.java
+++ src/org/funcito/Funcito.java
@@ -17,37 +17,64 @@ package org.funcito;
 
 import org.funcito.functionaljava.FJDelegate;
 import org.funcito.guava.GuavaDelegate;
+import org.funcito.internal.FuncitoDelegate;
 import org.funcito.jedi.JediDelegate;
 
 /**
  * This class is a public entry point into Funcito for the supported functional framework APIs.  Most users will probably
  * prefer to use static imports from the particular API being used, such as {@link FuncitoGuava}, {@link FuncitoFJ} (Functional Java),
  * or {@link FuncitoJedi}, because the usage is more terse.  But for rare cases when multiple functional frameworks are
- * being used within the same class, the static methods within this class can be used to differentiate between their usages.
+ * being used within the same class, the static methods within this class can be used to distinguish between them.
  * Example:
  * <p>
+ * <pre>
  * <code>
- *     import org.funcito.Funcito.*;<br/>
- *     <br/>
- *     MyClass guavaStub = guava().callsTo(MyClass.class);<br/>
- *     MyClass fjStub = fj().callsTo(MyClass.class);<br/>
- *     <br/>
- *     Function<MyClass,RetType> function = guava().functionFor(guavaStub.someMethod());<br/>
- *     F<MyClass,RetType> f = fj().fFor(fjStub.someMethod());<br/>
+ *     import org.funcito.Funcito.*;
+ *
+ *     Function<MyClass,RetType> function = guava().functionFor( callsTo(MyClass.class).someMethod());
+ *     Functor<MyClass,RetType>  functor  = jedi().functorFor( callsTo(MyClass.class).someMethod());
+ *     F<MyClass,RetType>        f        = fj().fFor( callsTo(MyClass.class).someMethod());
  * </code>
- * <p>
- * or calls may be made inline without problem, though it may be less readable to some:
- * <p>
- * <code>
- *     Function<MyClass,RetType> function = guava().functionFor( guava().callsTo(MyClass.class).someMethod());<br/>
- * </code>
+ * </pre>
  * @see FuncitoGuava
  * @see FuncitoFJ
  * @see FuncitoJedi
  */
 public class Funcito {
 
+    /**
+     * Creates a mock instance of a class, for the purpose of capturing and wrapping faux calls to the method or methods
+     * being wrapped.  Return values of these faux method calls should be passed as the argument to one of the functional-object
+     * factory methods, preferably inline so it is apparent in one place what method is being wrapped.  This <code>callsTo()</code>
+     * method is provided as a convenience when using the static delegate methods in this class.  It is functionally
+     * interchangeable with calls through to the delegates' version of <code>callsTo()</code>, such as <code>guava().callsTo()</code>
+     * @param clazz Class to be mocked
+     * @param <T> the specific type of the class to be mocked
+     * @return mock instance of clazz, solely for invoking and capturing faux method calls
+     */
+    public <T> T callsTo(Class<T> clazz) {
+        FuncitoDelegate defaultDelegate = new FuncitoDelegate();
+        return defaultDelegate.callsTo(clazz);
+    }
+
+    /**
+     * Delegate method used to help differentiate Guava from other Funcito supported functional frameworks
+     * @return a delegate with all of the same methods as the static versions in {@link FuncitoGuava}.
+     * @see FuncitoGuava
+     */
     public static GuavaDelegate guava() { return FuncitoGuava.delegate(); }
+
+    /**
+     * Delegate method used to help differentiate Functional Java from other Funcito supported functional frameworks
+     * @return a delegate with all of the same methods as the static versions in {@link FuncitoFJ}.
+     * @see FuncitoFJ
+     */
     public static FJDelegate fj() { return FuncitoFJ.delegate(); }
+
+    /**
+     * Delegate method used to help differentiate Jedi from other Funcito supported functional frameworks
+     * @return a delegate with all of the same methods as the static versions in {@link FuncitoJedi}.
+     * @see FuncitoJedi
+     */
     public static JediDelegate jedi() { return FuncitoJedi.delegate(); }
 }
